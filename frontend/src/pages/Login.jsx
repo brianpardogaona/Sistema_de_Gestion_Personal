2bcc6c0c395115447239290e9e84161a17ab4917
Brian <pardogaona.brian@gmail.com>
2025-03-14 17:51:09 +0100

    Enter para iniciar sesión | Animación mensaje de error

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import "../styles/login.css";
 import "../styles/styles.css";
 
-// functions
+// Functions
 import login from "@/pagesFunctions/login";
 import { useNavigate } from "react-router-dom";
 
@@ -13,6 +13,7 @@ function Login() {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const [passwordIsCorrect, setPasswordIsCorrect] = useState(true);
+  const [errorAnimation, setErrorAnimation] = useState("");
 
   useEffect(() => {
     document.body.classList.add("dark");
@@ -23,28 +24,44 @@ function Login() {
   }, []);
 
   const handleLogin = async () => {
-    const response = await login(username, password);
-
     try {
+      const response = await login(username, password);
       if (response.status === 200) {
         navigate("/inicio");
       } else {
         setPasswordIsCorrect(false);
+        setErrorAnimation("shake");
+        setTimeout(() => {
+          setErrorAnimation("");
+        }, 300);
       }
     } catch (e) {
       console.log(e);
     }
   };
 
+  const isLoginButtonVisible =
+    !passwordIsCorrect || (username !== "" && password !== "");
+
   return (
-    <div className="login-container montserrat-400">
+    <div
+      className="login-container montserrat-400"
+      onKeyDown={(e) => {
+        if (e.key === "Enter" && isLoginButtonVisible) {
+          handleLogin();
+        }
+      }}
+      tabIndex="0"
+    >
       <div id="left-side">
         <h2>¿Aún no tienes una cuenta?</h2>
         <Button id="register-button">Regístrate</Button>
       </div>
       <div id="right-side">
         <div id="title">
-          <h1 className="bold">BIENVENIDO/A A TU GESTOR DE PROGRESO PERSONAL</h1>
+          <h1 className="bold">
+            BIENVENIDO/A A TU GESTOR DE PROGRESO PERSONAL
+          </h1>
         </div>
         <div id="panel">
           <h2>Nombre de usuario</h2>
@@ -69,16 +86,22 @@ function Login() {
             onChange={(e) => setPassword(e.target.value)}
           />
 
-          <p className={!passwordIsCorrect ? "show bold warning" : "stash"}>
+          <p
+            className={`warning ${
+              !passwordIsCorrect ? `show ${errorAnimation}` : "stash"
+            }`}
+          >
             Las credenciales son incorrectas
           </p>
 
           <Button
             id="login-button"
             onClick={handleLogin}
-            className={!passwordIsCorrect || (username !== "" && password !== "")
-              ? "show"
-              : "stash"}
+            className={
+              !passwordIsCorrect || (username !== "" && password !== "")
+                ? "show"
+                : "stash"
+            }
           >
             Iniciar sesión
           </Button>
